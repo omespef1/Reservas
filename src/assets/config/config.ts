b6c3563d3718b1d8762c67f55cff96c403e426b2
omar pérez <omespef1@hotmail.com>
2018-08-09 15:42:36 -0500

    v
    v

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -1,8 +1,8 @@
-export const SERVICES_URL = 'http://186.154.240.180:8098/api/';
+//export const SERVICES_URL = 'http://186.154.240.180:8098/api/';
 export const appVersion = '18.0.8.0';
 export const appCopyright = 'SEVEN-ERP © 1992 - 2018';
 //export const SERVICES_URL = 'http://localhost/Nogalapi/api/';
-//export const SERVICES_URL = 'http://132.147.157.88/Nogalapi/api/';
+export const SERVICES_URL = 'http://132.147.157.88/Nogalapi/api/';
 export const developer = 'DigitalWare Team';
 export const developerMail = 'soporte@digitalware.com.co';
 export const developerFacebook = 'https://www.facebook.com/digitalware/';
