0f0c71f1fae3855df8d214d910e82adeffd2d4af
omespef1 <omespef1@hotmail.com>
2018-08-09 15:41:41 -0500

    v
    v

--- src/pages/confirm/confirm.ts
+++ src/pages/confirm/confirm.ts
@@ -56,7 +56,7 @@ export class ConfirmPage {
     let newBooking: any = {
       Emp_codi: 1,
       Res_fini: new Date(moment(this.booking.agend.startTime).toISOString()),
-      Res_fina: new Date(this.booking.agend.endTime).toISOString(),
+      Res_fina: new Date(moment(this.booking.agend.endTime).toISOString()),
       Soc_cont: this.user.Soc_cont,
       Mac_nume: this.user.Mac_nume1,
       Sbe_cont: this.user.Sbe_cont,
