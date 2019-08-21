f7af5772fafc35208cb814a68d4395fb31c3a303
omar pérez <omespef1@hotmail.com>
2019-08-20 19:13:53 -0500

    Versión 19.8.1 Apple
    Nuevas tabs, menú grande y splash.

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -8,7 +8,7 @@
  export const appCentralizacionUrl = "https://sac.digitalware.co:442/Seven_Crm/Centralizacion/api/";
 // const appCentralizacionUrl = "http://186.154.240.181/centralizacion/api/";
 export const appVersion:string = '19.0.8.1';
-export const appCopyright = 'SEVEN-ERP © 1992 - 2019';
+export const appCopyright = 'SEVEN-ERP © 1992-2019';
 export const appGooglePlayUrl = "https://play.google.com/store/apps/details?id=com.digitalware.com.co.seven.reservas";
 export const appAppStoreUrl = "https://itunes.apple.com/us/app/seven-reservas/id1426063835?l=es&ls=1&mt=8";
 export const developer = 'DigitalWare Team';
