759f02c9671e4ff732a5132fa4b4d9acb1b9b70a
omespef1 <omespef1@hotmail.com>
2020-10-22 23:58:00 -0500

    Versionamiento

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -7,7 +7,7 @@
  //export const appCentralizacionUrl = "http://consultoria.digitalware.co/seven_crm/centralizacion/api/";
  export const appCentralizacionUrl = "https://sac.digitalware.co:442/Seven_Crm/Centralizacion/api/";
 // const appCentralizacionUrl = "http://186.154.240.181/centralizacion/api/";
-export const appVersion:string = '20.0.10.3';
+export const appVersion:string = '20.0.10.4';
 export const appCopyright = 'SEVEN-ERP © 1992-2020';
 export const appGooglePlayUrl = "https://play.google.com/store/apps/details?id=com.digitalware.Clubes&hl=es";
 export const appAppStoreUrl = "https://apps.apple.com/co/app/nogal-reservas/id1089240574";
